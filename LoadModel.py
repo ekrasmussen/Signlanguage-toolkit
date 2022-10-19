abd13d8f29c3c80b17e2d6dadbba16f42cf6f8bf
marinadne ddd <marinladan@hotmail.com>
2022-10-19 10:39:20 +0200

    Fixed comments

--- LoadModel.py
+++ LoadModel.py
@@ -4,23 +4,23 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
-mp_holistic = mp.solutions.holistic  # Det er her vi introducerer vores holistiske model
-mp_drawing = mp.solutions.drawing_utils  # Her kommer der en mulighed for at tegne ved hjælp af drawing utilities
+mp_holistic = mp.solutions.holistic  # This is where we introduce our holistic model
+mp_drawing = mp.solutions.drawing_utils # It is possible to draw landmarks by the help of drawing_utils
 
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
-                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4) # Defining the number of landmarks in pose
     face = np.array([[res.x, res.y, res.z] for res in
-                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3)
+                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3) #Defining the number of landmarks in the face
     lh = np.array([[res.x, res.y, res.z] for res in
-                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3) # Defining the number of landmarks in left hand
     rh = np.array([[res.x, res.y, res.z] for res in
-                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros( # Defining the number of landmarks in the right hand
         21 * 3)
-    return np.concatenate([pose, face, lh, rh])
+    return np.concatenate([pose, face, lh, rh]) # Returns the number of landmarks
 
-def extract_keypoints_hands(results):
+def extract_keypoints_hands(results):  # Extracts the keypoints from the hands to be displayed
     lh = np.array([[res.x, res.y, res.z] for res in
                    results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
     rh = np.array([[res.x, res.y, res.z] for res in
@@ -30,49 +30,50 @@ def extract_keypoints_hands(results):
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image,
-            cv2.COLOR_BGR2RGB) # Her skifter vi farven på billedet fra Blå,grøn og rød til Rød, grøn og blå
-    image.flags.writeable = False  # Vi gør image unwriteable
+            cv2.COLOR_BGR2RGB) # Changing the color on the picture from BLUE-GREEN-RED TO RED-GREEN-BLUE
+    image.flags.writeable = False  # Makes the image unwriteable
     results = model.process(image)
-    image.flags.writeable = True  # Vi gør image writeable igen
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Her skifter vi det tilbage igen
+    image.flags.writeable = True  # Making the image writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Changes the color on the picture from RED-GREEN-BLUE to BLUE-GREEN-RED
     return image, results
 
 
 def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks,
-                              mp_holistic.FACEMESH_TESSELATION)  # Tegner ansigtet med landmarks
+                              mp_holistic.FACEMESH_TESSELATION)  # Draws the face with landmarks
     mp_drawing.draw_landmarks(image, results.pose_landmarks,
-                              mp_holistic.POSE_CONNECTIONS)  # Tegner din posering med landmarks
+                              mp_holistic.POSE_CONNECTIONS)  # Draws the pose with landmarks
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
-                              mp_holistic.HAND_CONNECTIONS)  # Tegner venstre hånd med landmarks
+                              mp_holistic.HAND_CONNECTIONS)  # Draws the left hand with landmarks
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks,
-                              mp_holistic.HAND_CONNECTIONS)  # Tegner højre hånd med landmarks
+                              mp_holistic.HAND_CONNECTIONS)  # Draws the right hand with landmarks
 
 
 def draw_styled_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
                               mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              # Tegner din posering med landmarks
+                              ) # Draws landmarks on the recognized face
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                              # Draws your pose with landmarks
                               mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
                               mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
                               )
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                               mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
                               mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
-                              )
+                              ) # Draws landmarks on the land hand
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                               mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
                               mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                              )
+                              ) # Draws landmarks on the right hand
 
 
-actions = np.array(['A','B','C','D','E','Idle'])
+actions = np.array(['A','B','C','D','E','Idle']) # Numpy array of our signs to detect
 
+# Adding the dense layers of our model
 model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(10, 126)))
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(10, 126))) # input sized is set to 10, since we want 10 frames to detect 
 model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
@@ -80,12 +81,12 @@ model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 model.summary()
 
-model.load_weights('epoch_400_frames_10.h5')
+model.load_weights('epoch_400_frames_10.h5') # Method to load the weights of our model
 
 
-colors = [(245,117,16), (117,245,16), (16,117,245), (245,117,16), (117,245,16), (16,117,245)]
+colors = [(245,117,16), (117,245,16), (16,117,245), (245,117,16), (117,245,16), (16,117,245)] # Colorcode for each correct predicition when reading
 def prob_viz(res, actions, input_frame, colors):
-    output_frame = input_frame.copy()
+    output_frame = input_frame.copy() # Sets the output frame to the current frame
     for num, prob in enumerate(res):
         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
         cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -93,9 +94,9 @@ def prob_viz(res, actions, input_frame, colors):
     return output_frame
 
 # 1. New detection variables
-sequence = []
-sentence = []
-threshold = 0.7
+sequence = [] # Empty List
+sentence = [] # Empty List
+threshold = 0.7 # Threshold for prediction
 
 cap = cv2.VideoCapture(0)
 # Set mediapipe model 
@@ -114,16 +115,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         
         # 2. Prediction logic
         keypoints = extract_keypoints_hands(results)
-#         #sequence.insert(0,keypoints)
-#         #sequence = sequence[:30]
         sequence.append(keypoints)
         sequence = sequence[-10:]
         
         if len(sequence) == 10:
-            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            res = model.predict(np.expand_dims(sequence, axis=0))[0] # This is where the model is set to predict 
             print(actions[np.argmax(res)])
             
-        #3. Viz logic
+        #3. Visualisation logic
             if res[np.argmax(res)] > threshold: 
                 if len(sentence) > 0: 
                     if actions[np.argmax(res)] != sentence[-1]:
@@ -134,7 +133,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             if len(sentence) > 5: 
                 sentence = sentence[-5:]
 
-            # Viz probabilities
+            # Visualisation probabilities
             image = prob_viz(res, actions, image, colors)
             
         cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
