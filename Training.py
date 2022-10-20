c97c36a869b3c3750da7d44107cf01d5e70c500f
Mathias Breiner <1089434@ucn.dk>
2022-10-20 14:11:12 +0200

    Cleaned up gui

--- Training.py
+++ Training.py
@@ -29,11 +29,11 @@ EPOCHS_AMOUNT = 2000
 #The seed used when spliting train and test data
 SEED = 1337
 
-def start(check_box_value, clicked, fps, epochs, seed):
+def start(check_box_value, clicked, frames, epochs, seed):
     actions = ACTIONSDICT[clicked.get()]
     try:    
         videos = count_videos(actions)
-        desired_length = fps.get()
+        desired_length = frames.get()
         epochs_amount = epochs.get()
         seed = seed.get()
 
@@ -45,86 +45,82 @@ def start(check_box_value, clicked, fps, epochs, seed):
         print('Check your values!')
 
 def select_directory():
-    directory = filedialog.askdirectory(title="Select directory for training data")
     global data_path
-    data_path = directory
+    directory = filedialog.askdirectory(title="Select directory for training data")
+    if directory:
+        data_path = directory
+        global label_directory_text
+        label_directory_text.set(data_path)
+
 
 def gui():
     root = tk.Tk()
+    
+    labelx = 50
+    spinboxx = 200
 
-    labelx = 100
-    spinboxx = 275
-
-    root.geometry("800x600")
+    root.geometry("400x300")
     root.title(f"Training & extract")
 
-    canvas = tk.Canvas(root, width=750, height=550)
+    canvas = tk.Canvas(root, width=400, height=300)
     canvas.pack()
 
     #Button for starting the process
-    start_button = tk.Button(root, text="Start", font=('Arial', 12), command=lambda : start(check_box_extract_value, clicked, desired_length, desired_epochs, desired_seed))
-    canvas.create_window(650, 450, window=start_button)
+    start_button = tk.Button(root, text="Start", font=('Arial', 10), command=lambda : start(check_box_extract_value, clicked, desired_length, desired_epochs, desired_seed))
+    canvas.create_window(370, 260, window=start_button, anchor=tk.E)
+
+    #Label that shows directory
+    global label_directory_text
+    label_directory_text = tk.StringVar()
+    label_directory_text.set(f'Directory: {data_path}')
+    label_directory = tk.Label(root, font=('Arial', 10), textvariable=label_directory_text)
+    canvas.create_window(30, 230, window=label_directory, anchor=tk.W)
 
     #Button for selecting directory for training data
-    directory_button = tk.Button(root, text="Select directory", font=('Arial', 12), command=select_directory)
-    canvas.create_window(500, 450, window=directory_button)
+    directory_button = tk.Button(root, text="Select directory", font=('Arial', 10), command=select_directory)
+    canvas.create_window(30, 260, window=directory_button, anchor=tk.W)
 
     #Extract checkbox
     #Used to get the value the check_box is in
     check_box_extract_value = tk.IntVar()
-    check_box_extract = tk.Checkbutton(root, text='Extract & train', variable=check_box_extract_value, onvalue=1, offvalue=0)
-    canvas.create_window(spinboxx, 200, window=check_box_extract)
+    check_box_extract = tk.Checkbutton(root, text='Extract & train', font=('Arial', 10), variable=check_box_extract_value, onvalue=1, offvalue=0)
+    canvas.create_window(spinboxx, 180, window=check_box_extract, anchor=tk.W)
 
     #Actions dropdown
-    label_actions = tk.Label(root, text="Actions to extract and/or train")
-    canvas.create_window(labelx,100,window=label_actions)
+    label_actions = tk.Label(root, text="Action set:", font=('Arial', 10))
+    canvas.create_window(labelx,60,window=label_actions, anchor=tk.W)
+
     dropdown_actions = ["Default", "Yubi-yay"]
     clicked = tk.StringVar()
     clicked.set(dropdown_actions[0])
     dropdown = tk.OptionMenu(root, clicked, *dropdown_actions)
-    canvas.create_window(spinboxx,100, window=dropdown)
+    canvas.create_window(spinboxx, 60, window=dropdown, anchor=tk.W)
 
     #Desired length
-    label_fps = tk.Label(root, text="Desired length/framerate")
-    canvas.create_window(labelx,125,window=label_fps)
+    label_fps = tk.Label(root, text="Frame amount:", font=('Arial', 10))
+    canvas.create_window(labelx,90,window=label_fps, anchor=tk.W)
+
     desired_length = tk.IntVar(value=1)
-    spinbox_fps = tk.Spinbox(root,textvariable=desired_length, from_= 1, to = 90)
-    canvas.create_window(spinboxx,125, window=spinbox_fps)
+    spinbox_frames = tk.Spinbox(root,textvariable=desired_length, from_= 1, to = 90)
+    canvas.create_window(spinboxx,90, window=spinbox_frames, anchor=tk.W)
 
     #Desired epochs
-    label_epochs = tk.Label(root, text="Amount of epochs")
-    canvas.create_window(labelx,150,window=label_epochs)
+    label_epochs = tk.Label(root, text="Epochs:", font=('Arial', 10))
+    canvas.create_window(labelx,120,window=label_epochs, anchor=tk.W)
+
     desired_epochs = tk.IntVar(value=1)
     spinbox_epochs = tk.Spinbox(root,textvariable=desired_epochs, from_= 1, to = 10000)
-    canvas.create_window(spinboxx,150, window=spinbox_epochs)
+    canvas.create_window(spinboxx,120, window=spinbox_epochs, anchor=tk.W)
 
     #Desired seed
-    label_seed = tk.Label(root, text="Select seed for training split")
-    canvas.create_window(labelx,175,window=label_seed)
+    label_seed = tk.Label(root, text="Seed:", font=('Arial', 10))
+    canvas.create_window(labelx,150,window=label_seed, anchor=tk.W)
+
     desired_seed = tk.IntVar(value=1)
     spinbox_seed = tk.Spinbox(root,textvariable=desired_seed, from_= 1, to=100000)
-    canvas.create_window(spinboxx,175, window=spinbox_seed)
+    canvas.create_window(spinboxx,150, window=spinbox_seed, anchor=tk.W)
 
     root.mainloop()
-
-
-#Make start button call a funktion that will starts training, and extracting if chosen on check_box
-
-
-# dropdown_options = ["Argh", "mah", ":)"]
-
-
-# # datatype of the menu text, also get 
-# clicked = tk.StringVar()
-# # initial menu text
-# clicked.set("Argh")
-
-# dropdown = tk.OptionMenu(root, clicked, *dropdown_options)
-# canvas.create_window(500, 300, window=dropdown)
-
-
-# test_entry = tk.Entry(root, font=('Arial', 12))
-# canvas.create_window(100, 100, window=test_entry)
 
 
 if __name__ == "__main__":
@@ -139,7 +135,7 @@ if __name__ == "__main__":
     else:
         #If you start the program with --extract behind it, it will extract data and train, if you start the program normally you only train.
         if args.extract:
-            extract_data(ACTIONS, VIDEO_AMOUNT, DESIRED_LENGTH, DATA_PATH)
+            extract_data(ACTIONS, VIDEO_AMOUNT, DESIRED_LENGTH, data_path)
             
-        model = YubiModel(DESIRED_LENGTH, SHAPE, ACTIONS, DATA_PATH)
+        model = YubiModel(DESIRED_LENGTH, SHAPE, ACTIONS, data_path)
         model.train_model(EPOCHS_AMOUNT, VIDEO_AMOUNT, SEED)
