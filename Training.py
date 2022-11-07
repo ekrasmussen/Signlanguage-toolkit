3f1464d921cfaf7bc899baaf7de6731e2ec282b3
marinaden750 <Marinladan@hotmail.com>
2022-11-07 13:17:14 +0100

    Trainer can now use Yubi-yay data set

--- Training.py
+++ Training.py
@@ -10,9 +10,9 @@ data_path= os.path.join('MP_Data')
 
 #actions we try to detect
 
-ACTIONSDICT = {"Default": np.array(['A', 'B', 'C', 'D', 'E', 'Idle']), "Yubi-yay": np.array(['A', 'B', 'C', 'D', 'E', 'Idle', 'Kosovo'])}
+ACTIONSDICT = {"Default": np.array(['A', 'B', 'C', 'D', 'E', 'Idle']), "Yubi-yay": np.array(['A', 'B', 'C', 'D', 'E', 'Idle','Dropper', 'Hue', 'Hvor', 'Jubilæum', 'Sejr'])}
 
-ACTIONS = ACTIONSDICT["Default"]
+actions = ACTIONSDICT["Default"]
 
 #Determents how many frames of the video is used
 DESIRED_LENGTH = 15
@@ -30,8 +30,14 @@ if __name__ == "__main__":
     parser.add_argument("--face", default=False, action="store_true")
     parser.add_argument("--pose",default=False, action="store_true")
     parser.add_argument('--path',type=str, required=False, default='Training_videos', help='path for Training videos (default "Training_videos")')
+    parser.add_argument("--actionset",type=str, required=False, default="default", help='Choose actions set (default "default")')
     args = parser.parse_args()
-    video_amount = count_videos(args.path, ACTIONS)
+    actionset = args.actionset
+    actionset = actionset.capitalize()
+    if actionset == "Yubi-yay":
+        actions = ACTIONSDICT["Yubi-yay"] 
+
+    video_amount = count_videos(args.path, actions)
 
     if args.gui:
         #If the gui argument is present, launch gui instead of tui
@@ -46,7 +52,7 @@ if __name__ == "__main__":
             shape = shape + 132
         
         if args.extract:
-            extract_data(ACTIONS, video_amount, DESIRED_LENGTH, data_path, shape, args.path)
+            extract_data(actions, video_amount, DESIRED_LENGTH, data_path, shape, args.path)
 
-        model = YubiModel(DESIRED_LENGTH, shape, ACTIONS, data_path)
+        model = YubiModel(DESIRED_LENGTH, shape, actions, data_path)
         model.train_model(EPOCHS_AMOUNT, video_amount, SEED)
