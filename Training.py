2547ae43c279dfdf7ff01f14c9dc31a1662f82ca
Mathias Breiner <1089434@ucn.dk>
2022-11-09 11:26:16 +0100

    Added args for desired_length, seed, and epochs

--- Training.py
+++ Training.py
@@ -12,16 +12,33 @@ data_path= os.path.join('MP_Data')
 
 ACTIONSDICT = {"Default": np.array(['A', 'B', 'C', 'D', 'E', 'Idle']), "Yubi-yay": np.array(['A', 'B', 'C', 'D', 'E', 'Idle','Dropper', 'Hue', 'Hvor', 'Jubilæum', 'Sejr'])}
 
-actions = ACTIONSDICT["Default"]
+def check_values(desired_length, seed, epochs_amount, actionset):
+     
+    failed = False
+    actions = np.array([])
 
-#Determents how many frames of the video is used
-DESIRED_LENGTH = 15
+    if not 0 < desired_length < 91:
+        failed = True
+        print('frames can only be between 1 to 90')
+    
+    if not 0 < seed < 100001:
+        failed = True
+        print('seed can only be between 1 to 100000')
+    
+    if not 0 < epochs_amount < 10001:
+        failed = True
+        print('epochs can only be between 1 to 10000')
 
-#Amount of epochs used when training
-EPOCHS_AMOUNT = 2000
+    actionset = actionset.capitalize()
+    if actionset == "Yubi-yay":
+        actions = ACTIONSDICT["Yubi-yay"]
+    elif actionset == "Default":
+        actions = ACTIONSDICT["Default"]
+    else:
+        failed = True
+        print(f'actionset is not valid. Valid options are {list(ACTIONSDICT.keys())}')
+    return failed, actions
 
-#The seed used when spliting train and test data
-SEED = 1337
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create and train a model based on a dataset with tensorflow")
@@ -29,30 +46,40 @@ if __name__ == "__main__":
     parser.add_argument("--gui", default=False, action="store_true")
     parser.add_argument("--face", default=False, action="store_true")
     parser.add_argument("--pose",default=False, action="store_true")
+    parser.add_argument("--frames", type=int, required=False, default=1, help='Amount of frames that are to be extracted from videos (default 1)')
+    parser.add_argument("--epochs", type=int, required=False, default=1, help='Amount of epochs used for training (default 1)')
+    parser.add_argument("--seed", type=int, required=False, default=1, help='The seed used to split test and training data (default 1)')
     parser.add_argument('--path',type=str, required=False, default='Training_videos', help='path for Training videos (default "Training_videos")')
-    parser.add_argument("--actionset",type=str, required=False, default="default", help='Choose actions set (default "default")')
+    parser.add_argument("--actionset",type=str, required=False, default="Default", help='Choose actions set (default "default")')
     args = parser.parse_args()
-    actionset = args.actionset
-    actionset = actionset.capitalize()
-    if actionset == "Yubi-yay":
-        actions = ACTIONSDICT["Yubi-yay"] 
-
-    video_amount = count_videos(args.path, actions)
 
     if args.gui:
         #If the gui argument is present, launch gui instead of tui
         gui = Gui(ACTIONSDICT, data_path)
         gui.start_gui()
     else:
-        #If you start the program with --extract behind it, it will extract data and train, if you start the program normally you only train.
-        shape = 126
-        if args.face:
-            shape = shape + 1404
-        if args.pose:
-            shape = shape + 132
-        
-        if args.extract:
-            extract_data(actions, video_amount, DESIRED_LENGTH, data_path, shape, args.path)
+        #Sets up variables from params
+        actionset = args.actionset
+        seed = args.seed
+        desired_length = args.frames
+        epochs_amount = args.epochs
+        path = args.path
 
-        model = YubiModel(DESIRED_LENGTH, shape, actions, data_path)
-        model.train_model(EPOCHS_AMOUNT, video_amount, SEED)
+        failed, actions = check_values(desired_length, seed, epochs_amount, actionset)
+    
+        if not failed: 
+            video_amount = count_videos(path, actions)
+            #If you start the program with --extract behind it, it will extract data and train, if you start the program normally you only train.
+            shape = 126
+            if args.face:
+                shape = shape + 1404
+            if args.pose:
+                shape = shape + 132
+            
+            if args.extract:
+                extract_data(actions, video_amount, desired_length, data_path, shape, path)
+
+            model = YubiModel(desired_length, shape, actions, data_path)
+            model.train_model(epochs_amount, video_amount, seed)
+        else:
+            print('Not started invalid values check fail message above')
