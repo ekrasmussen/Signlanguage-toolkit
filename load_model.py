cd3ecbf507d36eca34578f8cab1ace12ac3ffa6e
marinadne ddd <marinladan@hotmail.com>
2022-12-20 12:41:01 +0100

    Added comments for load_model

--- load_model.py
+++ load_model.py
@@ -2,7 +2,7 @@ import configparser
 import re
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, GRU
 
 
 class Model:
@@ -30,13 +30,16 @@ class Model:
 
     #Creates/loads model. file_path is the path to the h5 file 
     def load_model(self, file_path):
+        #               Original Model inspired by Nicolas Renottes action recognition guide
+        #               Source link: https://www.youtube.com/watch?v=doDUihpj6ro
+        #               Nicolas' Github: https://github.com/nicknochnack
         model = Sequential()
-        model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(self.desired_length, self.shape)))
-        model.add(LSTM(128, return_sequences=True, activation='relu'))
-        model.add(LSTM(64, return_sequences=False, activation='relu'))
+        model.add(GRU(64, return_sequences=True, activation='relu', input_shape=(self.desired_length, self.shape)))
+        model.add(GRU(128, return_sequences=True, activation='relu'))
+        model.add(GRU(64, return_sequences=False, activation='relu'))       #This line will have return_sequenecs set to False, because the next layer is a dense-layer so we need to not return the sequences to that layer
         model.add(Dense(64, activation='relu'))
         model.add(Dense(32, activation='relu'))
-        model.add(Dense(self.actions.shape[0], activation='softmax'))
+        model.add(Dense(self.actions.shape[0], activation='softmax'))       #This line is going to return values that are within 0-1 and with the sums of all values returned adding up to 1
         model.summary()
         model.load_weights(f'{file_path}.h5')
         return model
@@ -45,26 +48,26 @@ class Model:
     def sentence_update(self, res): 
         print(f'res {res}')
         if np.unique(self.predictions[-10:])[0]==np.argmax(res): 
-            if res[np.argmax(res)] > self.threshold: 
+            if res[np.argmax(res)] > self.threshold: #res[np.argmax(res)] is grabbing the threshold. Checks whether or not res is above threshold 
                 
-                if len(self.sentence) > 0: 
+                if len(self.sentence) > 0: #Checks if we have any sentence in variable sentence
                     if self.actions[np.argmax(res)] != self.sentence[-1]: #Checks that the predicted sign is not the same as the last one
-                        self.sentence.append(self.actions[np.argmax(res)])
+                        self.sentence.append(self.actions[np.argmax(res)]) #Append the initial action to the sentence array. If there is no sentences in the current array, then the current action cant match whats in the sentence
                 else:
                     self.sentence.append(self.actions[np.argmax(res)])
 
         if len(self.sentence) > 5: 
-            self.sentence = self.sentence[-5:]
+            self.sentence = self.sentence[-5:] #if len(self.sentence) is greater than 5, grab the last 5 values so we dont end up with giant array trying to render
 
     #Gives a prediction based on given keypoint.
     def predict(self, keypoints):
-        self.sequence.append(keypoints)
+        self.sequence.append(keypoints) #Appending keypoints to sequence
         self.sequence = self.sequence[-self.desired_length:]
         
         is_desired_length = False #Is used outside of method to check if res is empty
         res = []
-        if len(self.sequence) == self.desired_length:
-            res = self.model.predict(np.expand_dims(self.sequence, axis=0))[0]
+        if len(self.sequence) == self.desired_length: # If the length of self.squence is equal to self.desired_length, only then will there be made a prediction
+            res = self.model.predict(np.expand_dims(self.sequence, axis=0))[0] # the expand_dims function in NumPy is used to expand the shape of an input array that is passed to it. 
             print(self.actions[np.argmax(res)])
             self.predictions.append(np.argmax(res))
             is_desired_length = True
